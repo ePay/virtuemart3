866387f4ec87810c1849bee292dd3cb2cdd8f549
welrachid <wel@rachid.dk>
2015-08-05 10:37:25 +0200

    Removed author details
    Copied the file from standardplugin and removed the old author details
    since this is not relevant to this plugin

--- epay/tmpl/post_payment.php
+++ epay/tmpl/post_payment.php
@@ -1,21 +1,6 @@
 <?php
 defined ('_JEXEC') or die();
 
-/**
- * @author Valérie Isaksen
- * @version $Id$
- * @package VirtueMart
- * @subpackage payment
- * @copyright Copyright (C) 2004-Copyright (C) 2004-2015 Virtuemart Team. All rights reserved.   - All rights reserved.
- * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
- * VirtueMart is free software. This version may have been modified pursuant
- * to the GNU General Public License, and as distributed it includes or
- * is derivative of works licensed under the GNU General Public License or
- * other free or open source software licenses.
- * See /administrator/components/com_virtuemart/COPYRIGHT.php for copyright notices and details.
- *
- * http://virtuemart.net
- */
 		$html = '<table>' . "\n";
 		$html .= $this->getHtmlRow('EPAY_PAYMENT_NAME', $viewData['EPAY_PAYMENT_NAME']);
 		$html .= $this->getHtmlRow('EPAY_TRANSACTION_ID', $viewData['EPAY_TRANSACTION_ID']);
